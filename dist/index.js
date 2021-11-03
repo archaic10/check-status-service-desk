dbe939bc2c0890d21091e286c59fa3c56d598c4b
archaic10 <vitor.franca.9400@gmail.com>
2021-11-03 15:05:37 -0300

    test

--- dist/index.js
+++ dist/index.js
@@ -11483,29 +11483,8 @@ const axios = __nccwpck_require__(5462)
 var status = null
 var fileConfig = null
 async function run (){
-    
+    await loadFileConfig()
     try {
-        let github_token = core.getInput("GITHUB_TOKEN")
-        let path_file = core.getInput("path-file")
-        console.log("path_file") 
-        console.log(path_file) 
-        let octokit = github.getOctokit(github_token)
-        console.log("github.context.payload.repository")    
-        console.log(github.context.payload.repository)
-        console.log("github.context.payload.repository.owner.login")    
-        console.log(github.context.payload.repository.owner)   
-            
-        let {data} = await octokit.rest.repos.getContent({
-            owner: github.context.payload.repository.owner.login,
-            repo: github.context.payload.repository.name,
-            path: path_file,
-        })
-
-        let path = data.download_url
-        await setFileConfig(path)
-
-        console.log("run")
-        console.log(getFileConfig())
         if(checkRepoConfig (github.context, getFileConfig())){
             let basic_auth =  core.getInput("basic-auth")
             let url = core.getInput("url-jira")    
@@ -11521,22 +11500,28 @@ async function run (){
     }
 }
 
-// async function loadFileConfig(){
-//     try {           
-        
-//     } catch (error) {
-//         core.setFailed(error.message)
-//     }
-// }
+async function loadFileConfig(){
+    try {           
+        let github_token = core.getInput("GITHUB_TOKEN")
+        let path_file = core.getInput("path-file")        
+        let octokit = github.getOctokit(github_token)    
+        let {data} = await octokit.rest.repos.getContent({
+            owner: github.context.payload.repository.owner.login,
+            repo: github.context.payload.repository.name,
+            path: path_file,
+        })
+
+        let path = data.download_url
+        await setFileConfig(path)
+    } catch (error) {
+        core.setFailed(error.message)
+    }
+}
 
 async function setFileConfig(path){
     try {
-        console.log("setFileConfig")
-        console.log(path)
         await axios.get(path).then((res)=>{
-            fileConfig = res.data
-            console.log("fileConfig")      
-            console.log(fileConfig)      
+            fileConfig = res.data 
         }).catch((error)=>{
             console.log(error)
         })
@@ -11577,13 +11562,7 @@ function getStatus() {
     return status
 }
 
-function checkRepoConfig (context, config){
-    console.log("config")
-    console.log(config)
-    console.log("file config")
-    console.log(config.branch_head)
-    console.log("pull request object")
-    console.log(context.payload.pull_request.head)
+function checkRepoConfig (context, config){    
     if(context.payload.pull_request.head.ref == config.branch_head && context.payload.pull_request.base.ref == config.branch_base  && config != null){
         return true
     }        
--- index.js
+++ index.js
@@ -5,29 +5,8 @@ const axios = require('axios')
 var status = null
 var fileConfig = null
 async function run (){
-    
+    await loadFileConfig()
     try {
-        let github_token = core.getInput("GITHUB_TOKEN")
-        let path_file = core.getInput("path-file")
-        console.log("path_file") 
-        console.log(path_file) 
-        let octokit = github.getOctokit(github_token)
-        console.log("github.context.payload.repository")    
-        console.log(github.context.payload.repository)
-        console.log("github.context.payload.repository.owner.login")    
-        console.log(github.context.payload.repository.owner)   
-            
-        let {data} = await octokit.rest.repos.getContent({
-            owner: github.context.payload.repository.owner.login,
-            repo: github.context.payload.repository.name,
-            path: path_file,
-        })
-
-        let path = data.download_url
-        await setFileConfig(path)
-
-        console.log("run")
-        console.log(getFileConfig())
         if(checkRepoConfig (github.context, getFileConfig())){
             let basic_auth =  core.getInput("basic-auth")
             let url = core.getInput("url-jira")    
@@ -43,22 +22,28 @@ async function run (){
     }
 }
 
-// async function loadFileConfig(){
-//     try {           
-        
-//     } catch (error) {
-//         core.setFailed(error.message)
-//     }
-// }
+async function loadFileConfig(){
+    try {           
+        let github_token = core.getInput("GITHUB_TOKEN")
+        let path_file = core.getInput("path-file")        
+        let octokit = github.getOctokit(github_token)    
+        let {data} = await octokit.rest.repos.getContent({
+            owner: github.context.payload.repository.owner.login,
+            repo: github.context.payload.repository.name,
+            path: path_file,
+        })
+
+        let path = data.download_url
+        await setFileConfig(path)
+    } catch (error) {
+        core.setFailed(error.message)
+    }
+}
 
 async function setFileConfig(path){
     try {
-        console.log("setFileConfig")
-        console.log(path)
         await axios.get(path).then((res)=>{
-            fileConfig = res.data
-            console.log("fileConfig")      
-            console.log(fileConfig)      
+            fileConfig = res.data 
         }).catch((error)=>{
             console.log(error)
         })
@@ -99,13 +84,7 @@ function getStatus() {
     return status
 }
 
-function checkRepoConfig (context, config){
-    console.log("config")
-    console.log(config)
-    console.log("file config")
-    console.log(config.branch_head)
-    console.log("pull request object")
-    console.log(context.payload.pull_request.head)
+function checkRepoConfig (context, config){    
     if(context.payload.pull_request.head.ref == config.branch_head && context.payload.pull_request.base.ref == config.branch_base  && config != null){
         return true
     }        
