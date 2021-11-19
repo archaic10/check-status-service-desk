12e4992e81e31269d172c3e9439aee755c9b8851
archaic10 <vitor.franca.9400@gmail.com>
2021-11-19 13:42:39 -0300

    feat: version 4 release

--- dist/index.js
+++ dist/index.js
@@ -11481,13 +11481,16 @@ const github = __nccwpck_require__(6366)
 const axios = __nccwpck_require__(5462)
 
 var status = null
+var typeStatus = null
+var changeStatus = false
 async function run (){    
     try {        
       let basic_auth =  core.getInput("basic-auth")
-      let url = core.getInput("url-jira")    
+      let url = core.getInput("url-jira")
+      let otherTypesCurrentStatus = core.getInput("other-types-current-status")    
       let interval = setInterval(()=>{
         if(getStatus() == null || getStatus() != 'done')
-          setStatus(basic_auth, url)
+          setStatus(basic_auth, url, otherTypesCurrentStatus)
         if (getStatus() != null)
           checkStatus(interval)
       } , 30000)
@@ -11497,15 +11500,15 @@ async function run (){
 }
 
 async function checkStatus(interval) {   
-    if (getStatus() == 'done'){
+    if (getStatus() == 'done' || changeStatus ){
       clearInterval(interval)
       core.setOutput("result", "GMUD aprovada")
     }else{
-      console.log('pendente de aprovação')
+      console.log('Pendente de aprovação')
     }
 }
 
-async function setStatus(basic_auth, url) {
+async function setStatus(basic_auth, url, otherTypesCurrentStatus) {
     
     await axios.get(url,
       {
@@ -11515,6 +11518,14 @@ async function setStatus(basic_auth, url) {
 
       }).then((res) => {
         status = res.data.currentStatus.statusCategory.toLowerCase()
+        if(otherTypesCurrentStatus){
+          if(typeStatus == null)
+            setTypeStatus(res.data.currentStatus.status.toLowerCase())                     
+
+          if(typeStatus != res.data.currentStatus.status.toLowerCase())
+            changeStatus = true
+          
+        }
       }).catch((error) => {
         core.setFailed(error.message)
       })      
@@ -11522,6 +11533,26 @@ async function setStatus(basic_auth, url) {
  
 function getStatus() {
     return status
+}
+
+function setTypeStatus(nameStatus){
+    typeStatus = removeAccent(nameStatus)
+}
+
+function getTypeStatus(){
+  return typeStatus
+}
+
+function removeAccent (text)
+{       
+    text = text.toLowerCase();                                                         
+    text = text.replace(new RegExp('[ÁÀÂÃ]','gi'), 'a');
+    text = text.replace(new RegExp('[ÉÈÊ]','gi'), 'e');
+    text = text.replace(new RegExp('[ÍÌÎ]','gi'), 'i');
+    text = text.replace(new RegExp('[ÓÒÔÕ]','gi'), 'o');
+    text = text.replace(new RegExp('[ÚÙÛ]','gi'), 'u');
+    text = text.replace(new RegExp('[Ç]','gi'), 'c');
+    return text;                 
 }
 
 function checkRepoConfig (context){
